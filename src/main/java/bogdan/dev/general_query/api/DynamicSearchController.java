5aa5ac9c7b8e00c7312f5540d66ce0c20572b50b
bogdancstrike <bogdan.cstrike@yahoo.com>
2024-10-01 19:05:56 +0300

    added endpoint to return database metadatas (tables, columns)

--- src/main/java/bogdan/dev/general_query/api/DynamicSearchController.java
+++ src/main/java/bogdan/dev/general_query/api/DynamicSearchController.java
@@ -3,17 +3,11 @@ package bogdan.dev.general_query.api;
 import bogdan.dev.general_query.model.SearchRequest;
 import bogdan.dev.general_query.service.DynamicSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.sql.DataSource;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -38,6 +32,11 @@ public class DynamicSearchController {
         }
 
         return result;
+    }
+
+    @GetMapping("/metadata")
+    public Map<String, List<String>> getAllTablesAndColumns(@RequestParam(defaultValue = "false") boolean only_tables) throws SQLException {
+        return dynamicSearchService.getAllTablesAndColumns(only_tables);
     }
 
 }
--- src/main/java/bogdan/dev/general_query/service/DynamicSearchService.java
+++ src/main/java/bogdan/dev/general_query/service/DynamicSearchService.java
@@ -149,4 +149,30 @@ public class DynamicSearchService {
         tableMetadataCache.put(tableName, columnTypes);
         return columnTypes;
     }
+
+    public Map<String, List<String>> getAllTablesAndColumns(boolean onlyTables) throws SQLException {
+        Map<String, List<String>> tableColumnsMap = new HashMap<>();
+        DatabaseMetaData metaData = jdbcTemplate.getDataSource().getConnection().getMetaData();
+
+        try (ResultSet tables = metaData.getTables(null, null, "%", new String[]{"TABLE"})) {
+            while (tables.next()) {
+                String tableName = tables.getString("TABLE_NAME");
+
+                if (onlyTables) {
+                    // If only table names are requested, add an empty list for columns
+                    tableColumnsMap.put(tableName, new ArrayList<>());
+                } else {
+                    List<String> columns = new ArrayList<>();
+                    try (ResultSet columnsResultSet = metaData.getColumns(null, null, tableName, null)) {
+                        while (columnsResultSet.next()) {
+                            String columnName = columnsResultSet.getString("COLUMN_NAME");
+                            columns.add(columnName);
+                        }
+                    }
+                    tableColumnsMap.put(tableName, columns);
+                }
+            }
+        }
+        return tableColumnsMap;
+    }
 }
